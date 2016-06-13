269a18cf599297dafea3b3c067c78ccefe770f98
J12D <heinzderalligator@googlemail.com>
2016-06-13 14:48:11 +0200

    Update dependencies

--- utils/init_env.sh
+++ utils/init_env.sh
@@ -1,25 +1,34 @@
+#! /bin/bash
+
 # Install solc
-sudo add-apt-repository ppa:ethereum/ethereum-qt
-sudo add-apt-repository ppa:ethereum/ethereum
+yes | sudo add-apt-repository ppa:ethereum/ethereum-qt
+yes | sudo add-apt-repository ppa:ethereum/ethereum
 # sudo add-apt-repository ppa:ethereum/ethereum-dev
 sudo apt-get update
 sudo apt-get install cpp-ethereum
 
 # Install latest node.js
-mkdir $HOME/.local
-cd $HOME/.local
-wget https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
-tar xvzf node-v0.12.7-linux-x64.tar.gz
-cd $OLDPWD
+#mkdir $HOME/.local
+#cd $HOME/.local
+#wget https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
+#tar xvzf node-v0.12.7-linux-x64.tar.gz
+#cd $OLDPWD
+
+sudo apt-get install git
+sudo apt-get install nodejs
+sudo apt-get install npm
 
 # Install web3 and truffle
 sudo apt-get install git
-npm install -g web3
-npm install -g truffle
+sudo npm install -g web3
+sudo npm install -g truffle
 
 # Install testrpc
 sudo apt-get install python-dev
 sudo apt-get install python-pip
+sudo apt-get install pkg-config
+sudo apt-get install libffi-dev
+sudo pip install --upgrade setuptools
 sudo pip install pysha3
 sudo pip install repoze.lru
 sudo apt-get install libssl-dev
@@ -29,4 +38,5 @@ sudo pip install pbkdf2
 sudo pip install rlp
 sudo pip install bitcoin
 sudo pip install pyethash
-sudo pip install eth-testrpc
+sudo npm install -g ethereumjs-testrpc
+#sudo pip install eth-testrpc
